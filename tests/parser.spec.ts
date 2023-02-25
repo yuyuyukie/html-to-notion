289f6def96f0788c81a1ae6c9f37850b25949462
YukiYama <zx275fire@gmail.com>
2023-02-25 16:49:03 +0900

    refactor: group existing tests as e2e tests

--- tests/parser.spec.ts
+++ tests/parser.spec.ts
@@ -5,190 +5,192 @@ import rawHtmlTwo from './prodExample2/rawHtml';
 import expectedBlocksTwo from './prodExample2/expectedBlocks';
 
 describe('NotionParser', () => {
-  it('should parse paragraphs', () => {
-    const testHtml = '<p>text content</p>';
-    expect(parseHtmlToNotionBlocks(testHtml)).toStrictEqual([
-      {
-        object: 'block',
-        paragraph: {
-          rich_text: [{ text: { content: 'text content' }, type: 'text' }]
-        },
-        type: 'paragraph'
-      }
-    ]);
-  });
+  describe('e2e', () => {
+    it('should parse paragraphs', () => {
+      const testHtml = '<p>text content</p>';
+      expect(parseHtmlToNotionBlocks(testHtml)).toStrictEqual([
+        {
+          object: 'block',
+          paragraph: {
+            rich_text: [{ text: { content: 'text content' }, type: 'text' }]
+          },
+          type: 'paragraph'
+        }
+      ]);
+    });
 
-  it('should parse links to paragraphs', () => {
-    const testHtml = '<a href="https://notion.so">Click here</a>';
-    expect(parseHtmlToNotionBlocks(testHtml)).toStrictEqual([
-      {
-        object: 'block',
-        paragraph: {
-          rich_text: [
-            {
-              text: {
-                content: 'Click here',
+    it('should parse links to paragraphs', () => {
+      const testHtml = '<a href="https://notion.so">Click here</a>';
+      expect(parseHtmlToNotionBlocks(testHtml)).toStrictEqual([
+        {
+          object: 'block',
+          paragraph: {
+            rich_text: [
+              {
+                text: {
+                  content: 'Click here',
+                },
+                type: 'text'
+              }
+            ]
+          },
+          type: 'paragraph'
+        }
+      ]);
+    });
+
+    it('should parse headings', () => {
+      const testHtml =
+        '<h1>text content</h1><h2>text content</h2><h3>text content</h3><h4>text content</h4><h5>text content</h5><h6>text content</h6>';
+      const expectedBlocks = [
+        {
+          object: 'block',
+          heading_1: {
+            rich_text: [{ text: { content: 'text content' }, type: 'text' }]
+          },
+          type: 'heading_1'
+        },
+        {
+          object: 'block',
+          heading_2: {
+            rich_text: [{ text: { content: 'text content' }, type: 'text' }]
+          },
+          type: 'heading_2'
+        },
+        {
+          object: 'block',
+          heading_3: {
+            rich_text: [{ text: { content: 'text content' }, type: 'text' }]
+          },
+          type: 'heading_3'
+        },
+        {
+          object: 'block',
+          heading_3: {
+            rich_text: [{ text: { content: 'text content' }, type: 'text' }]
+          },
+          type: 'heading_3'
+        },
+        {
+          object: 'block',
+          heading_3: {
+            rich_text: [{ text: { content: 'text content' }, type: 'text' }]
+          },
+          type: 'heading_3'
+        },
+        {
+          object: 'block',
+          heading_3: {
+            rich_text: [{ text: { content: 'text content' }, type: 'text' }]
+          },
+          type: 'heading_3'
+        }
+      ];
+      expect(parseHtmlToNotionBlocks(testHtml)).toStrictEqual(expectedBlocks);
+    });
+
+    it('should parse a series of text paragraphs', () => {
+      const testHtml =
+        '<p>text1<span>text2</span>text3<strong>text4</strong></p>';
+      expect(parseHtmlToNotionBlocks(testHtml)).toStrictEqual([
+        {
+          object: 'block',
+          paragraph: {
+            rich_text: [
+              { text: { content: 'text1' }, type: 'text' },
+              { text: { content: ' text2' }, type: 'text' },
+              { text: { content: ' text3' }, type: 'text' },
+              { text: { content: ' text4' }, type: 'text' }
+            ]
+          },
+          type: 'paragraph'
+        }
+      ]);
+    });
+
+    it('should parse a multiple paragraph blocks', () => {
+      const testHtml = '<div><p>text1</p><p>text2</p><div>text3</div></div>';
+      expect(parseHtmlToNotionBlocks(testHtml)).toStrictEqual([
+        {
+          object: 'block',
+          paragraph: {
+            rich_text: [{ text: { content: 'text1' }, type: 'text' }]
+          },
+          type: 'paragraph'
+        },
+        {
+          object: 'block',
+          paragraph: {
+            rich_text: [{ text: { content: 'text2' }, type: 'text' }]
+          },
+          type: 'paragraph'
+        },
+        {
+          object: 'block',
+          paragraph: {
+            rich_text: [{ text: { content: 'text3' }, type: 'text' }]
+          },
+          type: 'paragraph'
+        }
+      ]);
+    });
+
+    it('should parse multiple elements type', () => {
+      const testHtml =
+        '<div><h1>Title</h1><p>text <a href="https://notion.so">link</a> text42.</p><p>Another text</p></div>';
+      expect(parseHtmlToNotionBlocks(testHtml)).toStrictEqual([
+        {
+          object: 'block',
+          heading_1: {
+            rich_text: [{ text: { content: 'Title' }, type: 'text' }]
+          },
+          type: 'heading_1'
+        },
+        {
+          object: 'block',
+          paragraph: {
+            rich_text: [
+              { text: { content: 'text' }, type: 'text' },
+              {
+                text: {
+                  content: ' link',
+                },
+                type: 'text'
               },
-              type: 'text'
-            }
-          ]
+              { text: { content: ' text42.' }, type: 'text' }
+            ]
+          },
+          type: 'paragraph'
         },
-        type: 'paragraph'
-      }
-    ]);
-  });
+        {
+          object: 'block',
+          paragraph: {
+            rich_text: [{ text: { content: 'Another text' }, type: 'text' }]
+          },
+          type: 'paragraph'
+        }
+      ]);
+    });
 
-  it('should parse headings', () => {
-    const testHtml =
-      '<h1>text content</h1><h2>text content</h2><h3>text content</h3><h4>text content</h4><h5>text content</h5><h6>text content</h6>';
-    const expectedBlocks = [
-      {
-        object: 'block',
-        heading_1: {
-          rich_text: [{ text: { content: 'text content' }, type: 'text' }]
-        },
-        type: 'heading_1'
-      },
-      {
-        object: 'block',
-        heading_2: {
-          rich_text: [{ text: { content: 'text content' }, type: 'text' }]
-        },
-        type: 'heading_2'
-      },
-      {
-        object: 'block',
-        heading_3: {
-          rich_text: [{ text: { content: 'text content' }, type: 'text' }]
-        },
-        type: 'heading_3'
-      },
-      {
-        object: 'block',
-        heading_3: {
-          rich_text: [{ text: { content: 'text content' }, type: 'text' }]
-        },
-        type: 'heading_3'
-      },
-      {
-        object: 'block',
-        heading_3: {
-          rich_text: [{ text: { content: 'text content' }, type: 'text' }]
-        },
-        type: 'heading_3'
-      },
-      {
-        object: 'block',
-        heading_3: {
-          rich_text: [{ text: { content: 'text content' }, type: 'text' }]
-        },
-        type: 'heading_3'
-      }
-    ];
-    expect(parseHtmlToNotionBlocks(testHtml)).toStrictEqual(expectedBlocks);
-  });
-
-  it('should parse a series of text paragraphs', () => {
-    const testHtml =
-      '<p>text1<span>text2</span>text3<strong>text4</strong></p>';
-    expect(parseHtmlToNotionBlocks(testHtml)).toStrictEqual([
-      {
-        object: 'block',
-        paragraph: {
-          rich_text: [
-            { text: { content: 'text1' }, type: 'text' },
-            { text: { content: ' text2' }, type: 'text' },
-            { text: { content: ' text3' }, type: 'text' },
-            { text: { content: ' text4' }, type: 'text' }
-          ]
-        },
-        type: 'paragraph'
-      }
-    ]);
-  });
-
-  it('should parse a multiple paragraph blocks', () => {
-    const testHtml = '<div><p>text1</p><p>text2</p><div>text3</div></div>';
-    expect(parseHtmlToNotionBlocks(testHtml)).toStrictEqual([
-      {
-        object: 'block',
-        paragraph: {
-          rich_text: [{ text: { content: 'text1' }, type: 'text' }]
-        },
-        type: 'paragraph'
-      },
-      {
-        object: 'block',
-        paragraph: {
-          rich_text: [{ text: { content: 'text2' }, type: 'text' }]
-        },
-        type: 'paragraph'
-      },
-      {
-        object: 'block',
-        paragraph: {
-          rich_text: [{ text: { content: 'text3' }, type: 'text' }]
-        },
-        type: 'paragraph'
-      }
-    ]);
-  });
-
-  it('should parse multiple elements type', () => {
-    const testHtml =
-      '<div><h1>Title</h1><p>text <a href="https://notion.so">link</a> text42.</p><p>Another text</p></div>';
-    expect(parseHtmlToNotionBlocks(testHtml)).toStrictEqual([
-      {
-        object: 'block',
-        heading_1: {
-          rich_text: [{ text: { content: 'Title' }, type: 'text' }]
-        },
-        type: 'heading_1'
-      },
-      {
-        object: 'block',
-        paragraph: {
-          rich_text: [
-            { text: { content: 'text' }, type: 'text' },
-            {
-              text: {
-                content: ' link',
-              },
-              type: 'text'
-            },
-            { text: { content: ' text42.' }, type: 'text' }
-          ]
-        },
-        type: 'paragraph'
-      },
-      {
-        object: 'block',
-        paragraph: {
-          rich_text: [{ text: { content: 'Another text' }, type: 'text' }]
-        },
-        type: 'paragraph'
-      }
-    ]);
-  });
-
-  xit('should handle HTML found in production', () => {
-    console.log(JSON.stringify(parseHtmlToNotionBlocks(rawHtmlOne).map(value => ({
-      type: value.type,
-      object: value.object,
-    // @ts-ignore
-      ...value[value.type]
-    }))));
-    console.log(JSON.stringify(expectedBlocksOne.map((value => ({
-      type: value.type,
-      object: value.object,
-    // @ts-ignore
-      ...value[value.type]
-    })))));
-    expect(parseHtmlToNotionBlocks(rawHtmlOne)).toStrictEqual(
-      expectedBlocksOne
-    );
-    expect(parseHtmlToNotionBlocks(rawHtmlTwo)).toStrictEqual(
-      expectedBlocksTwo
-    );
+    xit('should handle HTML found in production', () => {
+      console.log(JSON.stringify(parseHtmlToNotionBlocks(rawHtmlOne).map(value => ({
+        type: value.type,
+        object: value.object,
+        // @ts-ignore
+        ...value[value.type]
+      }))));
+      console.log(JSON.stringify(expectedBlocksOne.map((value => ({
+        type: value.type,
+        object: value.object,
+        // @ts-ignore
+        ...value[value.type]
+      })))));
+      expect(parseHtmlToNotionBlocks(rawHtmlOne)).toStrictEqual(
+        expectedBlocksOne
+      );
+      expect(parseHtmlToNotionBlocks(rawHtmlTwo)).toStrictEqual(
+        expectedBlocksTwo
+      );
+    });
   });
 });
