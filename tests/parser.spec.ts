3d9a5d7d5b08e4b51e0e4e06e22642096bad96c5
YukiYama <zx275fire@gmail.com>
2023-02-26 23:51:14 +0900

    test: drop anchor test

--- tests/parser.spec.ts
+++ tests/parser.spec.ts
@@ -110,6 +110,25 @@ describe('NotionParser', () => {
         }
       ]);
     });
+    xit('should parse links to paragraphs', () => {
+      const testHtml = '<a href="https://notion.so">Click here</a>';
+      expect(parseHtmlToNotionBlocks(testHtml)).toStrictEqual([
+        {
+          object: 'block',
+          paragraph: {
+            rich_text: [
+              {
+                text: {
+                  content: 'Click here'
+                },
+                type: 'text'
+              }
+            ]
+          },
+          type: 'paragraph'
+        }
+      ]);
+    });
   });
   describe('simple blocks', () => {
     describe('br', () => {
@@ -181,26 +200,6 @@ describe('NotionParser', () => {
           object: 'block',
           paragraph: {
             rich_text: [{ text: { content: 'text content' }, type: 'text' }]
-          },
-          type: 'paragraph'
-        }
-      ]);
-    });
-
-    it('should parse links to paragraphs', () => {
-      const testHtml = '<a href="https://notion.so">Click here</a>';
-      expect(parseHtmlToNotionBlocks(testHtml)).toStrictEqual([
-        {
-          object: 'block',
-          paragraph: {
-            rich_text: [
-              {
-                text: {
-                  content: 'Click here'
-                },
-                type: 'text'
-              }
-            ]
           },
           type: 'paragraph'
         }
